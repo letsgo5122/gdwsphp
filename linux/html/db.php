d3fa9d0f711f9708300ab3e4e50ffa9ce51b4536
letsgo5122 <letsgo5122@gmail.com>
2020-08-16 22:23:26 +0800

    web3

--- linux/html/db.php
+++ linux/html/db.php
@@ -1,8 +1,8 @@
 <?php
-$dbuser = 'labxuqgojbd123';
-$dbpass = '16171458e5a062b7ec9541a3290626221ec11daeef6c0c2fa96fa1bf5db70123';
+$dbuser = 'labxuqgojbd234';
+$dbpass = '16171458e5a062b7ec9541a3290626221ec11daeef6c0c2fa96fa1bf5db70234';
 $host = 'ec2-35-175-155-248.compute-1.amazonaws.com';
-$dbname='dc4hmb1sbma123';
+$dbname='dc4hmb1sbma234';
 $port="5432";
 $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$dbuser;password=$dbpass;sslmode=require";
 try{
